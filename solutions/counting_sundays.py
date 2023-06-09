b64c3e406f1871252f99fcc911ad76aed363072b
vandervel <72418762+vandervel@users.noreply.github.com>
2023-06-09 11:40:19 -0700

    added

---
+++ solutions/counting_sundays.py
@@ -0,0 +1,36 @@
+# Number of sundays that fell on the first of the month in the twentieth century.
+
+
+months = [31,
+         28,
+         31,
+         30,
+         31,
+         30,
+         31,
+         31,
+         30,
+        31,
+         30,
+        31]
+
+
+def count_sundays():
+    
+    day = 1
+    sunday_count = 0
+    
+    for i in range(1901, 2001):
+        
+        for j in months:
+            
+            day += j
+            if day % 4 == 0:
+                day += 1
+            if day % 7 == 0:
+                sunday_count += 1
+                
+    return sunday_count
+
+print(count_sundays())
+                
