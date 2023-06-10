b4c8bbcce0b28c68f21cfc145b24196f2bb48b46
vandervel <72418762+vandervel@users.noreply.github.com>
2023-06-09 17:51:11 -0700

    added

---
+++ solutions/problem22.py
@@ -0,0 +1,44 @@
+# Each name in a list is assigned a score based on 
+# the sum of the letters in the name where each
+# letter is worth it's position in the alphabet in
+# points, i.e. 'COLIN' is assigned
+# the score of 3 + 15 + 12 + 9 + 14 = 53
+# Find the sum of scores for all names in the file names.txt
+
+
+import pandas as pd
+import string
+
+f = open("p022_names.txt", 'r')
+names = sorted(f.read().replace('"','').split(','), key=str)
+total_score = 0
+
+alpha = string.ascii_lowercase.upper()
+alpha_scores = dict(list(enumerate(alpha)))
+alpha_scores = {k: j for j, k in alpha_scores.items()}
+
+for i in alpha_scores:
+    alpha_scores[i] += 1
+    
+
+def letter_score(name, scores):
+    
+    scr = 0
+    name_len = len(name)
+    
+    for i in range(name_len):
+        scr += alpha_scores[name[i]]
+        
+    return scr
+  
+def name_score(name, index):
+    
+    return index * letter_score(name)
+  
+
+
+for i, j in enumerate(names):
+    
+    total_score += name_score(j, i+1)
+
+print(total_score)
